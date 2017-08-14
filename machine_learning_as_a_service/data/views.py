d34a0cd8b6a2f571d6831022cbc307b50c70d8d3
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-08-14 13:55:21 +0300

    Get CSV URL from post

--- machine_learning_as_a_service/data/views.py
+++ machine_learning_as_a_service/data/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def save_csv_as_dataframe(request):
     print("Save CSV as DataFrame")
+
+    if (request.POST):
+        # Get CSV URL from post; default to None if not provided
+        csv_url = request.POST.get('csv_url', None)
