9463bfa5070d88c510c1ed2c760181f7bd2f2ec2
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-08-14 14:51:01 +0300

    Def __str__ representation

--- machine_learning_as_a_service/data/models.py
+++ machine_learning_as_a_service/data/models.py
@@ -6,3 +6,9 @@ class Data(models.Model):
     data_frame = PickledObjectField()
 
     source_url = models.URLField(null=True)
+
+    def __str__(self):
+        if (self.source_url):
+            return self.source_url
+        else:
+            return 'unknown source'
